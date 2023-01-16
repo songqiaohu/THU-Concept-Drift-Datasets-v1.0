260c0ddea502dd5d5e6b2ed0d006b0b42d20968b
songqiaohu <songqiaohu@163.com>
2023-01-16 21:54:20 +0800

    delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from DataStreamGenerator import DataStreamGenerator
-from skmultiflow.data import DataStream
-
-
-C = DataStreamGenerator(class_count=2, attribute_count=2, sample_count=100000, noise=True, redunce_variable=True)
-X, Y = C.Nonlinear_Sudden_RollingTorus(plot=True, save=True)
-stream = DataStream(X, Y)
-print(stream.next_sample(100))
